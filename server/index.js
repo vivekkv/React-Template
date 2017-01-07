de1887fecf0db74404c75c36861d8201de9d5ed1
vivek kv <vivek kv>
2017-01-07 12:44:25 +0530

    server file change

--- server/index.js
+++ server/index.js
@@ -8,55 +8,57 @@ var {
   port
 } = require('./common/config')
 
-/** DEV MODE */
+// /** DEV MODE */
 
-const webpack = require('webpack')
-const webpackMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const config = require('../webpack.config.js')
+// const webpack = require('webpack')
+// const webpackMiddleware = require('webpack-dev-middleware')
+// const webpackHotMiddleware = require('webpack-hot-middleware')
+// const config = require('../webpack.config.js')
 
-const compiler = webpack(config)
-const middleware = webpackMiddleware(compiler, {
-  publicPath: config.output.publicPath,
-  contentBase: 'src',
-  stats: {
-    colors: true,
-    hash: false,
-    timings: true,
-    chunks: false,
-    chunkModules: false,
-    modules: false
-  }
-})
+// const compiler = webpack(config)
+// const middleware = webpackMiddleware(compiler, {
+//   publicPath: config.output.publicPath,
+//   contentBase: 'src',
+//   stats: {
+//     colors: true,
+//     hash: false,
+//     timings: true,
+//     chunks: false,
+//     chunkModules: false,
+//     modules: false
+//   }
+// })
 
-app.use(middleware)
-app.use(webpackHotMiddleware(compiler))
-app.get('/', function response(req, res) {
-  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')))
-  res.end()
-})
+// app.use(middleware)
+// app.use(webpackHotMiddleware(compiler))
+// app.get('/', function response(req, res) {
+//   res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')))
+//   res.end()
+// })
 
-/* END */
+// /* END */
 
 app.use('/assets', express.static(path.join(__dirname, '../app/static/assets')))
 app.use(express.static("dist"))
 app.use("/map", mapRoutes)
 
-db.connect('mongodb://localhost:27017/FromMeToU', function (err) {
+// db.connect('mongodb://localhost:27017/FromMeToU', function (err) {
+//   if (err) {
+//     console.log('Unable to connect to Mongo.')
+//   } else {
+
+    
+//   }
+// })
+
+var server = app.listen(port, host, (err) => {
   if (err) {
-    console.log('Unable to connect to Mongo.')
-  } else {
-
-    var server = app.listen(port, host, (err) => {
-      if (err) {
-        return onError(err)
-      }
-      console.log("SERVER STARTED !")
-    })
-
-    var socket = require("socket.io")
-    var io = socket(server)
-    require('./socket').init(io)
-    console.log("MONGO DB CONNECTED !")
+    return onError(err)
   }
+  console.log("SERVER STARTED !")
 })
+
+var socket = require("socket.io")
+var io = socket(server)
+require('./socket').init(io)
+console.log("MONGO DB CONNECTED !")
