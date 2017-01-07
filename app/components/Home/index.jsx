71f6a49188f6d63ba5030dbcd793c831fa20ce85
vivek kv <vivek kv>
2017-01-07 14:41:05 +0530

    style change

--- app/components/Home/index.jsx
+++ app/components/Home/index.jsx
@@ -10,7 +10,7 @@ class Home extends React.Component {
     render() {
         return (<div className={styles.wrapper}>
             <Navigation />
-            <Content {...this.props}/>
+            
         </div>)
     }
 }
