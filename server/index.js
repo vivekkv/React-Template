7849777ed1037ebdee03ff521df772df3d357f72
vivek kv <vivek kv>
2017-01-07 12:05:31 +0530

    server config for heroku

--- server/index.js
+++ server/index.js
@@ -4,28 +4,28 @@ var app = express()
 var { host, port } = require('./common/config')
 var custResponses = require('./middlewares/customResponses')
 
-/** DEV MODE */
-const webpack = require('webpack')
-const webpackMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const config = require('../webpack.config.js')
-const compiler = webpack(config)
-const middleware = webpackMiddleware(compiler, {
-publicPath: config.output.publicPath,
-contentBase: 'src',
-stats: {
-colors: true,
-hash: false,
-timings: true,
-chunks: false,
-chunkModules: false,
-modules: false
-}
-})
-/* END */
+// /** DEV MODE */
+// const webpack = require('webpack')
+// const webpackMiddleware = require('webpack-dev-middleware')
+// const webpackHotMiddleware = require('webpack-hot-middleware')
+// const config = require('../webpack.config.js')
+// const compiler = webpack(config)
+// const middleware = webpackMiddleware(compiler, {
+// publicPath: config.output.publicPath,
+// contentBase: 'src',
+// stats: {
+// colors: true,
+// hash: false,
+// timings: true,
+// chunks: false,
+// chunkModules: false,
+// modules: false
+// }
+// })
+// /* END */
 
-app.use(middleware)
-app.use(webpackHotMiddleware(compiler))
+// app.use(middleware)
+// app.use(webpackHotMiddleware(compiler))
 app.get('/', function response(req, res) {
 res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')))
 res.end()
